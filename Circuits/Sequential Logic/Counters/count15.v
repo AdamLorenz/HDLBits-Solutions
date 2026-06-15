1dcf8b2c2554f5cb25a90168752ba68d3120d6d7
AdamLorenz <79378265+AdamLorenz@users.noreply.github.com>
2026-06-15 14:20:41 -0500

    Create count15.v

---
+++ Circuits/Sequential Logic/Counters/count15.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output [3:0] q);
+    always @(posedge clk) begin
+        if(reset == 1 | q == 4'b1111) begin
+            q <= '0;
+        end else begin
+            q <= q + 1'b1;
+        end
+    end
+endmodule
