3da4c2a4d037224ef03b3b6b158ea51923cbab40
AdamLorenz <79378265+AdamLorenz@users.noreply.github.com>
2026-06-16 20:55:30 -0500

    Update and rename Circuits/Sequential Logic/Shift Registers/Rule90.v to Circuits/Sequential Logic/Shift Registers/More Circuits/Rule90.v

--- Circuits/Sequential Logic/Shift Registers/More Circuits/Rule90.v
+++ Circuits/Sequential Logic/Shift Registers/More Circuits/Rule90.v
@@ -7,11 +7,15 @@ module top_module(
         if(load) begin
             q <= data;
         end else begin
+            // A better way is to use vector part select and concatenation in a single operation:
+            q <= {1'b0, q[511:1]} ^ {q[510:0], 1'b0} ;
+            /*
             q[0] <= 1'b0 ^ q[1];
             for (int i = 1; i < 511; i++) begin
                 q[i] <= q[i - 1] ^ q[i + 1];
             end
             q[511] <= 1'b0 ^ q[510];
+            */
         end
     end
 endmodule
