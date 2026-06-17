64d75e3f3248e8528b4908f2a8e28e037fbd8812
AdamLorenz <79378265+AdamLorenz@users.noreply.github.com>
2026-06-16 21:26:07 -0500

    Create Rule110.v

---
+++ Circuits/Sequential Logic/Shift Registers/More Circuits/Rule110.v
@@ -0,0 +1,12 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q
+); 
+    always @(posedge clk) begin
+        if (load) q <= data;
+        else q <= (q[511:0] & ~{1'b0, q[511:1]}) | (q[511:0] ^ {q[510:0], 1'b0});
+        // L: {1'b0, q[511:1]} C: q[511:0] R: {q[510:0], 1'b0}
+    end
+endmodule
