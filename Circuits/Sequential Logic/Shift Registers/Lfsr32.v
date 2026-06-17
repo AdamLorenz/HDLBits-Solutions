30ddb0137352e46095ae86662bdbee495ef31ebb
AdamLorenz <79378265+AdamLorenz@users.noreply.github.com>
2026-06-16 19:25:34 -0500

    Create Lfsr32.v

---
+++ Circuits/Sequential Logic/Shift Registers/Lfsr32.v
@@ -0,0 +1,15 @@
+module top_module(
+    input clk,
+    input reset,    // Active-high synchronous reset to 32'h1
+    output reg [31:0] q
+); 
+    always @(posedge clk) begin
+        if (reset) q <= 32'h1;
+        else begin
+            q <= {q[0],q[31:1]};
+            q[21] <= q[0] ^ q[22];
+            q[1] <= q[0] ^ q[2];
+            q[0] <= q[0] ^ q[1];
+        end
+    end
+endmodule
