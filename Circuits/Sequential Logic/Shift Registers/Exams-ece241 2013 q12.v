2a6b3127657a26518f738579b03702dc26684de8
AdamLorenz <79378265+AdamLorenz@users.noreply.github.com>
2026-06-16 20:32:21 -0500

    Create Exams-ece241 2013 q12.v

---
+++ Circuits/Sequential Logic/Shift Registers/Exams-ece241 2013 q12.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input enable,
+    input S,
+    input A, B, C,
+    output Z ); 
+    reg [0:7] q;
+    always @(posedge clk) begin
+        if (enable) begin
+            q <= {S, q[0:6]};
+        end
+    end
+    assign Z = q[{A,B,C}];
+endmodule
