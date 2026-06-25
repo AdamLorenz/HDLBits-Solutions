81f3e0cdf5ebd4153135c31e61d16edceed8ad3b
AdamLorenz <79378265+AdamLorenz@users.noreply.github.com>
2026-06-25 15:44:27 -0500

    Create Exams-2014 q3bfsm.v

---
+++ Circuits/Sequential Logic/Finite State Machines/Exams-2014 q3bfsm.v
@@ -0,0 +1,32 @@
+module top_module (
+    input clk,
+    input reset,   // Synchronous reset
+    input x,
+    output z
+);
+    parameter 	A = 3'd0,
+                B = 3'd1,
+                C = 3'd2,
+                D = 3'd3,
+                E = 3'd4;
+    reg  [2:0] state;
+    wire [2:0] next_state;
+    
+    always @(*) begin
+        case(state)
+            A: 	next_state = x ? B : A;
+            B: 	next_state = x ? E : B;
+            C: 	next_state = x ? B : C;
+            D: 	next_state = x ? C : B;
+            E: 	next_state = x ? E : D;
+        endcase
+    end
+    
+    always @(posedge clk) begin
+        if (reset) 	state <= A;
+        else		state <= next_state;
+    end
+    
+    assign z = (state == D) | (state == E);
+    
+endmodule
