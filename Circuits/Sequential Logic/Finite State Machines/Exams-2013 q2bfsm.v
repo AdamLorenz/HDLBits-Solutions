faa1b7529e593c115460decf9bd7a3767bd789fa
AdamLorenz <79378265+AdamLorenz@users.noreply.github.com>
2026-06-26 20:59:33 -0500

    Create Exams-2013 q2bfsm.v

---
+++ Circuits/Sequential Logic/Finite State Machines/Exams-2013 q2bfsm.v
@@ -0,0 +1,43 @@
+module top_module (
+    input clk,
+    input resetn,    // active-low synchronous reset
+    input x,
+    input y,
+    output f,
+    output g
+); 
+	parameter 	A = 4'd0,
+                B = 4'd1,
+                C = 4'd2,
+                D = 4'd3,
+                E = 4'd4,
+                F = 4'd5,
+                G = 4'd6,
+                H = 4'd7,
+    			I = 4'd8;
+    reg  [4:0] state;
+    wire [4:0] next_state;
+    
+    always @(*) begin
+        case(state)
+            A: 	next_state = B;
+            B:	next_state = C;
+            C: 	next_state = x ? D : C;
+            D: 	next_state = x ? D : E;
+            E:	next_state = x ? F : C;
+            F:	next_state = y ? I : G;
+            G: 	next_state = y ? I : H;
+            H: 	next_state = H;
+            I: 	next_state = I;
+        endcase
+    end
+    
+    always @(posedge clk) begin
+        if (~resetn)	state <= A;
+        else			state <= next_state;
+    end
+
+    assign f = (state == B);
+    assign g = (state == F) | (state == G) | (state == I);
+        
+endmodule
